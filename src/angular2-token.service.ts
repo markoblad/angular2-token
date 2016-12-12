7e7775a3308e75fc20831ae34c2121bb8fec5bdf
valcucodev <githubdev@valcu.co>
2016-12-12 18:03:03 -0500

    fix extra slash in oAuth url

--- src/angular2-token.service.ts
+++ src/angular2-token.service.ts
@@ -111,7 +111,6 @@ export class Angular2TokenService implements CanActivate {
             oAuthPaths: {
                 github:                 'auth/github'
             },
-            oAuthCallbackPath:          'oauth_callback',
             oAuthWindowType:            'newWindow',
             globalOptions: {
                 headers: {
@@ -516,7 +515,7 @@ export class Angular2TokenService implements CanActivate {
     private _buildOAuthUrl(oAuthPath: string, callbackUrl: string, windowType: string): string {
         let url: string;
 
-        url = `${this._constructApiPath() || window.location.origin}/${oAuthPath}`;
+        url = `${this._constructApiPath() || (window.location.origin + '/')}${oAuthPath}`;
         url += `?omniauth_window_type=${windowType}`;
         url += `&auth_origin_url=${encodeURIComponent(callbackUrl)}`;
         if (this._currentUserType != null) {
